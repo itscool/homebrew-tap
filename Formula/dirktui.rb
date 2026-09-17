f34a212e1d7533915a541b10812b062daf9dd198
Scott Williams <scott@Scotts-MacBook-Pro.local>
2026-09-17 02:29:40 -0700

    Add dirktui formula 0.1.3

--- Formula/dirktui.rb
+++ Formula/dirktui.rb
@@ -1,9 +1,9 @@
 class Dirktui < Formula
   desc "Column file browser for the shell: dirk cd's, opens, and hands paths to the command line"
   homepage "https://github.com/itscool/homebrew-tap"
-  url "https://github.com/itscool/homebrew-tap/releases/download/dirktui-v0.1.2/dirktui-0.1.2-arm64.tar.gz"
-  sha256 "876f59af0e65b114f14b2b2955926b4099cb08f4acf857f2d7040d3f4526807a"
-  version "0.1.2"
+  url "https://github.com/itscool/homebrew-tap/releases/download/dirktui-v0.1.3/dirktui-0.1.3-arm64.tar.gz"
+  sha256 "9fc5157bb690e1dc4c98b1ae6059f41439d0a12b2b4a44b696c9748b5be160dc"
+  version "0.1.3"
 
   depends_on arch: :arm64
   depends_on :macos
